a73e83606b3625f1f59aa85ee4b5a6c147c48032
Dante de Araújo <danteeng@hotmail.com>
2025-01-02 20:46:03 -0300

    :art: fix(routes): all routes are prepended with /api/v1/

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set '/api' as the global prefix for all controllers
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
