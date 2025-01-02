2fe6966f865254bb15b4257f4c21785116a484a5
Dante de Araújo <danteeng@hotmail.com>
2025-01-02 20:51:05 -0300

    :recycle: refactor(app): adjust app module and remove old code from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientService } from './client.service';
+import { OrganizationService } from './organization.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly clientService: ClientService,
+    private readonly organizationService: OrganizationService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/clients')
+  getClients(): any {
+    return this.clientService.clients({});
+  }
+
+  @Post('/client')
+  createClient(): any {
+    // return this.clientService.createClient({
+    //   name: 'Dante',
+    //   phoneNumber: '+5585996105145',
+    //   email: 'danteeng@hotmail.com',
+    // });
+  }
+
+
+  @Get('/organizations')
+  listOrganizations(): any {
+    return this.organizationService.organizations({});
+  }
+  @Post('/organization')
+  createOrganization(): any {
+    return this.organizationService.createOrganization({
+      name: 'Drops',
+      email: 'drops@hotmail.com',
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { OrganizationService } from './organization.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
   imports: [ConfigModule.forRoot(), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, OrganizationService],
 })
 export class AppModule {}
